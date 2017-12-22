d0f420e5cdfea75d8e11e7c3e2f2c1f9153fa296
Unknown <preda_2006@hotmaiL.com>
2017-12-22 19:51:18 +0100

    AddingPackage + basic login/register

---
+++ app/src/main/java/be/henallux/masi/pedagogique/activities/loginRegisterActivities/RegisterPrompt.java
@@ -0,0 +1,15 @@
+package be.henallux.masi.pedagogique.activities.loginRegisterActivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import be.henallux.masi.pedagogique.R;
+
+public class RegisterPrompt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_prompt);
+    }
+}
