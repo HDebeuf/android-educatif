018db9b16aafb929116cf44eb7ebae1d2906b343
HDebeuf <32390605+HDebeuf@users.noreply.github.com>
2018-01-05 03:01:27 +0100

    more stable thread management + moving google maps feature + cut soundpool on record
    TO DO add provided data from pédas

--- app/src/main/java/be/henallux/masi/pedagogique/activities/musicalActivity/makeMusic/handlers/ISoundPoolHandler.java
+++ app/src/main/java/be/henallux/masi/pedagogique/activities/musicalActivity/makeMusic/handlers/ISoundPoolHandler.java
@@ -1,6 +1,10 @@
 package be.henallux.masi.pedagogique.activities.musicalActivity.makeMusic.handlers;
 
 
+import android.media.SoundPool;
+
+import java.util.ArrayList;
+
 /**
  * Created by hendrikdebeuf2 on 1/01/18.
  */
@@ -12,5 +16,13 @@ public interface ISoundPoolHandler {
     int loadSample(String fileName, String fileType);
 
     void playSample(int soundID);
+
+    SoundPool getSoundPool();
+
+    void stop(int soundId);
+
+    ArrayList<Integer> getStreamSoundIdList();
+
+    void setStreamSoundIdList(ArrayList<Integer> streamSoundIdList);
 
 }
